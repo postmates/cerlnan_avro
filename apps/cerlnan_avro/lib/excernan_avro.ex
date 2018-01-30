002841f5d71032dd9dfb31d8748a83629706bc24
John Koenig <john@calledshot.org>
2018-01-30 12:56:35 -0600

    Document cerlnan_avro (#6)
    Pulls documentation out of source into a specific README for
    cerlnan_avro.  What documentation existed before has been made
    more complete.

--- apps/cerlnan_avro/lib/excernan_avro.ex
+++ apps/cerlnan_avro/lib/excernan_avro.ex
@@ -3,29 +3,6 @@ defmodule ExCernan.Avro do
     Client / connection pool for Cernan's Avro source.
 
     This module is a pass-through to the cerlnan_avro application.
-    On startup, a number of poolbay backed connection pools are created
-    based on Application env.  If no pools are configured by the user, a
-    default pool equivalent to the following config is created:
-
-        ```
-          config :cerlnan_avro,
-            pools: [
-              {:cerlnan_avro,
-               %{}
-              }
-            ]
-        ```
-
-    Pool configs are of the form {pool-id :, pool-args} when pool-id: atom,
-    pool-args: map.  pool-args has the following form:
-
-        %{
-          :backend => Backend socket semantics.  Default = :cerlnan_avro_socket_v1.
-          :backend_args => Map of backend specific arguments.  Default = %{}.
-          :pool_size => Size of the connection pool to Cernan.  Default = 10.
-          :pool_overflow => Number of overflow workers allowed.  Default = 20.
-          ...
-        }
   """
 
   @doc """
